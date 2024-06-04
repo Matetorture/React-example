9af222af5437ea4f2f162da56c81c63ab6877824
Matetorture <przemek.pytlarz18@gmail.com>
2024-06-04 17:30:37 +0200

    Added form with validation

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,38 +1,85 @@
+import './Form.css';
 import React, { useState } from 'react';
 
 function Form() {
-  const [name, setName] = useState({ firstName: 'Jan', lastName: 'K' });
+  const [input, setInput] = useState({ firstName: '', lastName: '' });
+  const [errors, setErrors] = useState({ firstName: '', lastName: '' });
 
   const handleChange = (event) => {
-    setName({ ...name, [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    setInput({ ...input, [name]: value });
+
+    const newErrors = { ...errors };
+    switch (name) {
+      case 'firstName':
+        if (value.length < 3) {
+          newErrors.firstName = 'Imię musi mieć co najmniej 3 znaki';
+        } else if (value.length > 64) {
+          newErrors.firstName = 'Imię może mieć maksymalnie 64 znaki';
+        } else if (/[\d\W_]/.test(value)) {
+          newErrors.firstName = 'Imię nie może zawierać cyfr ani znaków specjalnych';
+        } else {
+          newErrors.firstName = '';
+        }
+        break;
+      case 'lastName':
+        if (value.length < 3) {
+          newErrors.lastName = 'Nazwisko musi mieć co najmniej 3 znaki';
+        } else if (value.length > 64) {
+          newErrors.lastName = 'Nazwisko może mieć maksymalnie 64 znaki';
+        } else if (/[\d\W_]/.test(value)) {
+          newErrors.lastName = 'Nazwisko nie może zawierać cyfr ani znaków specjalnych';
+        } else {
+          newErrors.lastName = '';
+        }
+        break;
+      default:
+        break;
+    }
+    setErrors(newErrors);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (errors.firstName || errors.lastName || input.firstName === '' || input.lastName === '') {
+      return;
+    }
+
+    console.log('Dane przesłane:', input);
   };
 
   return (
     <div>
-      <form>
-        <label htmlFor="firstName">First name</label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">Imię</label>
         <input
           type="text"
           name="firstName"
-          value={name.firstName}
+          value={input.firstName}
           onChange={handleChange}
         />
-
         <br/>
-        <br/>
+        {errors.firstName && <span className="error">{errors.firstName}</span>}
 
-        <label htmlFor="lastName">Last name</label>
+        <br />
+        <br />
+
+        <label htmlFor="lastName">Nazwisko</label>
         <input
           type="text"
           name="lastName"
-          value={name.lastName}
+          value={input.lastName}
           onChange={handleChange}
         />
-      </form>
-      
-      <br/>
+        <br/>
+        {errors.lastName && <span className="error">{errors.lastName}</span>}
 
-      <span>{name.firstName} {name.lastName}</span>
+        <br />
+        <br />
+
+        <button type="submit">Wyślij</button>
+      </form>
     </div>
   );
 }
